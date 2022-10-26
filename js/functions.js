49bf9d417aa5701e18cf9f6d63b55a2204775add
Noah Estes <estes2ns@dukes.jmu.edu>
2022-10-26 12:57:26 -0400

    finished

--- js/functions.js
+++ js/functions.js
@@ -7,16 +7,16 @@ function calculateTotal(q, p) {
 }
 
 function outputCartRow(item, total) {
-    //console.log(item, total);
     const { product: {title, filename, price}, quantity } = item;
     console.log(title, filename, price, quantity);
 
-    const row =`            <tr>
+    const row =`
+                <tr>
                     <td><img src="images/${filename}"></td>
                     <td>${title}</td>
                     <td>${quantity}</td>
-                    <td>${price}</td>
-                    <td>${total}</td>
+                    <td>$${price}</td>
+                    <td>$${total.toFixed(2)}</td>
                 </tr>`;
     document.write(row);
 
@@ -29,10 +29,62 @@ function outputTableBody() {
     }
 }
 
+function outputSubtotal() {
+    const row = `
+                <tr class="totals">
+                    <td colspan="4">Subtotal</td>
+                    <td>$${calculateSubtotal().toFixed(2)}</td>
+                </tr>`;
+    document.write(row);
+}
 
+function calculateSubtotal() {
+    let subtotal = 0;
+    for (let item of cart) {
+        console.log(subtotal += calculateTotal(item.quantity, item.product.price));
+    }
+    return subtotal;
+}
 
+function outputTax() {
+    const row = `
+                <tr class="totals">
+                    <td colspan="4">Tax</td>
+                    <td>$${calculateTax().toFixed(2)}</td>
+                </tr>`;
+    document.write(row);
+}
 
+function calculateTax() {
+    return calculateSubtotal() * tax_rate;
+}
 
+function outputShipping() {
+    const row = `
+                <tr class="totals">
+                    <td colspan="4">Shipping</td>
+                    <td>$${calculateShipping().toFixed(2)}</td>
+                </tr>`;
+    document.write(row);
+}
 
+function calculateShipping() {
+    if (calculateSubtotal() > shipping_threshold) {
+        return 0;
+    } else {
+        return 40;
+    }
+}
 
-        
+function outputGrandTotal() {
+    const row = `
+                <tr class="totals">
+                    <td colspan="4" class="focus">Grand Total</td>
+                    <td class="focus">$${calculateGrand().toFixed(2)}</td>
+                </tr>`;
+    document.write(row);
+}
+
+function calculateGrand() {
+    return calculateSubtotal() + calculateTax() + calculateShipping();
+}
